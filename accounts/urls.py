0cb002717bd106c33fab0054335fab79b4cd5dc1
BrianArchibald <brianallenarchibald@gmail.com>
2019-03-06 19:19:24 -0600

    forgot the regex path for login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login'),
+    re_path('login/.*', views.login, name='login'),
     path('logout', views.logout, name='logout'),
 ]
